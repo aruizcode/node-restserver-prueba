1bcb30cd6e76db43368cf7b99a96b5652db91f4a
adrian <adrianruizpicazo@gmail.com>
2018-11-27 19:01:44 +0100

    login normal

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autentication');
 const app = express();
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken ,(req, res) => {
 
     let desde = req.query.desde || 2;
     desde=Number(desde);
@@ -32,7 +33,7 @@ app.get('/usuario', function (req, res) {
 
           });
 });
-app.post('/usuario', function (req, res) {
+app.post('/usuario',[verificaToken , verificaAdmin_Role] , function (req, res) {
 
   let body = req.body;
 
@@ -60,7 +61,7 @@ app.post('/usuario', function (req, res) {
   });
 
 });
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id',[verificaToken , verificaAdmin_Role] , function (req, res) {
 
   let id = req.params.id;
   let body = _.pick(req.body , ['nombre','email','img','role','estado']);
@@ -81,7 +82,7 @@ app.put('/usuario/:id', function (req, res) {
     });
   });
 });
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id',[verificaToken , verificaAdmin_Role]  , function (req, res) {
 
     let id = req.params.id;
     let CambiarEstado = {
@@ -107,7 +108,10 @@ app.delete('/usuario/:id', function (req, res) {
       }
       res.json({
         ok : true,
-        usuarioBorrado
+        usuarioBorrado,
+        info : {
+          message : 'usuario borrado correctamente'
+        }
       });
     });
 });
--- server/server.js
+++ server/server.js
@@ -11,8 +11,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+//configuracion global de rutas
+app.use(require( './routes/index' ));
 
-app.use(require( './routes/usuario.js' ));
 
 
 mongoose.connect(process.env.URLDB,(err,res) => {
